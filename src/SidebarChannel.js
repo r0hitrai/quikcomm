05816db48d0c8fb31045fb09283b2dd84dc11e5a
Rohit Rai <rohitraijob@gmail.com>
2022-07-10 01:13:51 +0530

    SidebarChannel.js: set channel info on click

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -2,12 +2,24 @@
 import React from 'react';
 // import SidebarChannel.css from current folder
 import './SidebarChannel.css';
+// import use dispatch component from redux
+import { useDispatch } from 'react-redux';
+// import set channel info component from app slice
+import { setChannelInfo } from './features/appSlice';
 
 // SidebarChannel function, parameters: user id, channel name
 function SidebarChannel({ id, channelName }) {
+  const dispatch = useDispatch();
   // return HTML code
   return (
-    <div className="sidebarChannel">
+    <div className="sidebarChannel" 
+      onClick={() => dispatch(
+        setChannelInfo({
+          channelId: id,
+          channelName: channelName,
+        })
+      )}
+    >
       <h4>
         <span className="sidebarChannel__hash">#</span>{channelName}
       </h4>
