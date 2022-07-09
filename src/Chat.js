ee51455089f3caf664c092a9e8a15c056f880ab1
Rohit Rai <rohitraijob@gmail.com>
2022-07-10 01:20:22 +0530

    Chat.js: show selected channel name

--- src/Chat.js
+++ src/Chat.js
@@ -14,13 +14,23 @@ import GifIcon from '@mui/icons-material/Gif';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 // import Message component from Message.js
 import Message from './Message.js';
+// import use selector component from redux
+import { useSelector } from 'react-redux';
+// import select channel id and select channel name from app slice
+import { selectChannelId, selectChannelName } from './features/appSlice';
+// import select user component from user slice
+import { selectUser } from './features/userSlice';
 
 // Chat function
 function Chat() {
+  // user, channel id, channel name variables
+  const user = useSelector(selectUser);
+  const channelId = useSelector(selectChannelId);
+  const channelName = useSelector(selectChannelName);
   // return HTML code
   return (
     <div className="chat">
-      <ChatHeader />
+      <ChatHeader channelName={channelName}/>
       <div className="chat__messages">
         <Message />
         <Message />
