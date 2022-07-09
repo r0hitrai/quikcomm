c7f7f8feb57147aff32fb6dc7af1a38f9db306f0
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 23:41:13 +0530

    Login.js, Login.css: create

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+// import React component from react library
+import React from 'react';
+// import Login.css from current folder
+import './Login.css';
+
+// Login function
+function Login() {
+  // return HTML code
+  return (
+    <div className="login">
+      <h2>i am the login page.</h2>
+    </div>
+  );
+}
+
+// export Login component
+export default Login;
