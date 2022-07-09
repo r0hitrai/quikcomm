a6b11f39cb5156e4add4f72968b98963a33fb3a5
Rohit Rai <rohitraijob@gmail.com>
2022-07-10 00:20:02 +0530

    App.js: login, logout function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import React component from react library
-import React from 'react';
+import React, { useEffect } from 'react';
 // import App.css from current folder
 import './App.css';
 // import sidebar function from Sidebar.js
@@ -7,15 +7,41 @@ import Sidebar from './Sidebar';
 // import Chat function from Chat.js
 import Chat from './Chat';
 // import use selector component from redux
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 // import select user selector from user slice
-import { selectUser } from './features/userSlice';
+import { selectUser, login, logout } from './features/userSlice';
 // import Login componen from Login.js
 import Login from './Login';
+// import auth from firebase.js
+import { auth } from './firebase';
 
 function App() {
+  // use to shoot data into data layer
+  const dispatch = useDispatch();
   // logged in user
   const user = useSelector(selectUser);
+  // run on every render and update
+  useEffect(() => {
+    // on use of authentication
+    auth.onAuthStateChanged((authUser) => {
+      console.log("user is ", authUser);
+      // check for user
+      if (authUser) {
+        // the user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        )
+      } else {
+        // the user is logged out
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
   // return HTML code
   return (
     <div className="app">
