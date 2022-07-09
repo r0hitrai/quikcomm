86201e42a5b27b4cb674b8e0052387662f6412ea
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 18:54:52 +0530

    Message.js: add avatar

--- src/Message.js
+++ src/Message.js
@@ -2,13 +2,15 @@
 import React from 'react';
 // import Message.css from current folder
 import './Message.css';
+// import Avatar component from material ui
+import { Avatar } from '@mui/material';
 
 // Message function
 function Message() {
   // return HTML code
   return (
     <div className="message">
-
+      <Avatar />
     </div>
   );
 }
