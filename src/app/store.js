f0fc33aff85b69d9d76d2b9fe28969cc14599653
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 23:14:13 +0530

    store.js: import app reducer from app slice, export it

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import user reducer component from features folder
 import userReducer from '../features/userSlice';
+// import app reducer component from features folder
+import appReducer from '../features/appSlice';
 
 // export configureStore for user data layer
 export default configureStore({
+  // set reducer parameter
   reducer: {
     user: userReducer,
+    app: appReducer,
   },
 });
