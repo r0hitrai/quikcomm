09a72248573f374790295b261f95c10cbbf9624d
Rohit Rai <rohitraijob@gmail.com>
2022-07-10 00:34:33 +0530

    Sidebar.js: user Id

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -68,7 +68,7 @@ function Sidebar() {
         <Avatar src={user.photo}/>
         <div className="sidebar__profileInfo">
           <h3>{user.displayName}</h3>
-          <p>#thisIsMyId</p>
+          <p>#{user.uid.substring(0, 5)}</p>
         </div>
         <div className="sidebar__profileIcons">
           <MicIcon />
