3426786f556d4467fe1ab2ec7c97dc2b006ab1dc
Rohit Rai <raistudying@gmail.com>
2022-07-09 12:13:48 +0530

    Sidebar.js: heading

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,14 @@
 // import React component from react library
 import React from 'react'
 // import Sidebar.css from current folder
-import "./Sidebar.css"
+import './Sidebar.css'
 
 // Sidebar function
 function Sidebar() {
     // return value
     return (
-        <div>
-
+        <div className="sidebar">
+          <h2>i am the sidebar</h2>
         </div>
     )
 }
