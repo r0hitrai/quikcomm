dadf95afb6096e15c7ba9719c8e2e764ad5e0a5d
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 23:51:02 +0530

    Login.js: sign in button

--- src/Login.js
+++ src/Login.js
@@ -2,15 +2,22 @@
 import React from 'react';
 // import Login.css from current folder
 import './Login.css';
+// import Button component from material ui
+import { Button } from '@mui/material';
 
 // Login function
 function Login() {
+  // sign in function
+  const signIn = () => {
+    // google login
+  }
   // return HTML code
   return (
     <div className="login">
       <div className="login__logo">
         <img src="https://raw.githubusercontent.com/r0hitrai/Images/main/small_logo_blurple_RGB.png" alt="QuikComm" />
       </div>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 }
