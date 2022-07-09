830e4c21a1acf15975d5a0a5fbca4f8f75c8d529
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 16:10:52 +0530

    Chat.js: create, import in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// import React component from react
+// import React component from react library
 import React from 'react';
 // import App.css from current folder
 import './App.css';
 // import sidebar function from Sidebar.js
 import Sidebar from './Sidebar';
+// import Chat function from Chat.js
+import Chat from './Chat';
 
 function App() {
   // return HTML code
   return (
     <div className="app">
-      {/* Sidebar */}
       <Sidebar />
+      {/* Chat */}
+      <Chat />
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,15 @@
+// import React component from react library
+import React from 'react';
+
+// Chat function
+function Chat() {
+  // return HTML code
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+// export Chat function
+export default Chat;
