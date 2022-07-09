61cfb4746cccaa9d51ab90ec69c7492264cd0e56
Rohit Rai <rohitraijob@gmail.com>
2022-07-09 18:42:36 +0530

    create Message.js and Message.css

---
+++ src/Message.js
@@ -0,0 +1,17 @@
+// import React component from react library
+import React from 'ract';
+// import Message.css from current folder
+import './Message.css';
+
+// Message function
+function Message() {
+  // return HTML code
+  return (
+    <div className="message">
+
+    </div>
+  );
+}
+
+// export Message function
+export default Message;
