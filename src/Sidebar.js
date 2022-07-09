d93a9c9d6cbef9c9856c86526a52203cba38f7bc
Rohit Rai <rohitraijob@gmail.com>
2022-07-10 00:26:44 +0530

    Sidebar.js: get user data

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -22,56 +22,62 @@ import MicIcon from '@mui/icons-material/Mic';
 import HeadsetIcon from '@mui/icons-material/Headset';
 // import settings icon from material ui
 import SettingsIcon from '@mui/icons-material/Settings';
+// import use selector component from redux
+import { useSelector } from 'react-redux';
+// import select user component from user slice
+import { selectUser } from './features/userSlice';
 
 // Sidebar function
 function Sidebar() {
-    // return HTML code
-    return (
-        <div className="sidebar">
-          <div className="sidebar__top">
-            <h3>Sidebar Top</h3>
+  // user variable
+  const user = useSelector(selectUser);
+  // return HTML code
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Sidebar Top</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
             <ExpandMoreIcon />
+            <h4>Text Channels</h4>
           </div>
-          <div className="sidebar__channels">
-            <div className="sidebar__channelsHeader">
-              <div className="sidebar__header">
-                <ExpandMoreIcon />
-                <h4>Text Channels</h4>
-              </div>
-              <AddIcon className="sidebar__addChannel" />
-            </div>
-            <div className="sidebar__channelsList">
-              <SidebarChannel />
-              <SidebarChannel />
-              <SidebarChannel />
-              <SidebarChannel />
-            </div>
-          </div>
-          <div className="sidebar__voice">
-            <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large" />
-            <div className="sidebar__voiceInfo">
-              <h3>Voice Connected</h3>
-              <p>Stream</p>
-            </div>
-            <div className="sidebar__voiceIcons">
-              <InfoOutlindedIcon />
-              <CallIcon />
-            </div>
-          </div>
-          <div className="sidebar__profile">
-            <Avatar src="https://raw.githubusercontent.com/r0hitrai/Images/main/coder1.png"/>
-            <div className="sidebar__profileInfo">
-              <h3>Username</h3>
-              <p>#thisIsMyId</p>
-            </div>
-            <div className="sidebar__profileIcons">
-              <MicIcon />
-              <HeadsetIcon />
-              <SettingsIcon />
-            </div>
-          </div>
+          <AddIcon className="sidebar__addChannel" />
         </div>
-    );
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large" />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className="sidebar__voiceIcons">
+          <InfoOutlindedIcon />
+          <CallIcon />
+        </div>
+      </div>
+      <div className="sidebar__profile">
+        <Avatar src="https://raw.githubusercontent.com/r0hitrai/Images/main/coder1.png"/>
+        <div className="sidebar__profileInfo">
+          <h3>Username</h3>
+          <p>#thisIsMyId</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 // export Sidebar function
