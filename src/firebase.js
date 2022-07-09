bd82f3e10330914b9cb560757d9e4009445c6609
Rohit Rai <raistudying@gmail.com>
2022-07-09 11:16:26 +0530

    add and deploy project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRAi2ZJ975WLePJuONoE8HPe3rK-O6MSs",
+  authDomain: "quikcomm-435ea.firebaseapp.com",
+  projectId: "quikcomm-435ea",
+  storageBucket: "quikcomm-435ea.appspot.com",
+  messagingSenderId: "314028914608",
+  appId: "1:314028914608:web:f5eb8ec2f3b3363aad4875",
+  measurementId: "G-BV88VVFZZ5"
+};
