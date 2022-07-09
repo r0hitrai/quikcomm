ced8178762d78d9d01ecdf3dbcf344beb5aee6af
Rohit Rai <raistudying@gmail.com>
2022-07-09 11:58:47 +0530

    Sidebar.js: initial code

---
+++ src/Sidebar.js
@@ -0,0 +1,15 @@
+// import React component from react library
+import React from 'react'
+
+// Sidebar function
+function Sidebar() {
+    // return value
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+// export Sidebar function
+export default Sidebar
